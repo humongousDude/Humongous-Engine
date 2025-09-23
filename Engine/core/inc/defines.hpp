6770e04bab8ef9a376babe82887c5e1d01a18bb6
HumongousDude <youssefmelad1986@gmail.com>
2025-09-23 17:01:45 +0300

    cleaned up defines.hpp

--- Engine/core/inc/defines.hpp
+++ Engine/core/inc/defines.hpp
@@ -2,43 +2,37 @@
 #include <cstdint>
 
 // unsigned ints
-typedef uint8_t  u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
 
 // signed ints
-typedef int8_t  s8;
-typedef int16_t s16;
-typedef int32_t s32;
-typedef int64_t s64;
+using s8 = int8_t;
+using s16 = int16_t;
+using s32 = int32_t;
+using s64 = int64_t;
 
 // floats
-typedef float  f32;
-typedef double f64;
+using f32 = float;
+using f64 = double;
 
 // bool
-typedef int  b32;
-typedef char b8;
+using b8 = bool;
+using b32 = u32;
 
-#if defined(__clang__) || defined(__gcc__)
-#define STATIC_ASSERT _Static_assert
-#else
-#define STATIC_ASSERT static_assert
-#endif
+static_assert(sizeof(u8) == 1, "Expected u8 to be 1 byte.");
+static_assert(sizeof(u16) == 2, "Expected u16 to be 2 bytes.");
+static_assert(sizeof(u32) == 4, "Expected u32 to be 4 bytes.");
+static_assert(sizeof(u64) == 8, "Expected u64 to be 8 bytes.");
 
-STATIC_ASSERT(sizeof(u8) == 1, "Expected u8 to be 1 byte.");
-STATIC_ASSERT(sizeof(u16) == 2, "Expected u16 to be 2 bytes.");
-STATIC_ASSERT(sizeof(u32) == 4, "Expected u32 to be 4 bytes.");
-STATIC_ASSERT(sizeof(u64) == 8, "Expected u64 to be 8 bytes.");
+static_assert(sizeof(s8) == 1, "Expected s8 to be 1 byte.");
+static_assert(sizeof(s16) == 2, "Expected s16 to be 2 bytes.");
+static_assert(sizeof(s32) == 4, "Expected s32 to be 4 bytes.");
+static_assert(sizeof(s64) == 8, "Expected s64 to be 8 bytes.");
 
-STATIC_ASSERT(sizeof(s8) == 1, "Expected s8 to be 1 byte.");
-STATIC_ASSERT(sizeof(s16) == 2, "Expected s16 to be 2 bytes.");
-STATIC_ASSERT(sizeof(s32) == 4, "Expected s32 to be 4 bytes.");
-STATIC_ASSERT(sizeof(s64) == 8, "Expected s64 to be 8 bytes.");
-
-STATIC_ASSERT(sizeof(f32) == 4, "Expected f32 to be 4 bytes.");
-STATIC_ASSERT(sizeof(f64) == 8, "Expected f64 to be 8 bytes.");
+static_assert(sizeof(f32) == 4, "Expected f32 to be 4 bytes.");
+static_assert(sizeof(f64) == 8, "Expected f64 to be 8 bytes.");
 
 /* #ifdef HEXPORT
 // exports
